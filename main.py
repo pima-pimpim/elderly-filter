f8c5868db3a33d5cf17585c8c19854fb1b1100de
pima-pimpim <pimpisa.chrcht@gmail.com>
2025-04-08 19:17:11 +0700

    init

--- main.py
+++ main.py
@@ -8,23 +8,28 @@ st.set_page_config(
 
 st.title("Modgut's Elderly Project")
 st.write('Filter data to find healthy people with following criteria:')
-st.markdown('Filter data to find healthy people with following criteria:')
 
-region = pd.read_csv('data.csv')
+df = pd.read_csv('data.csv')
+col_list = df.columns.to_list()
 
-option = st.selectbox()
+for i in range(len(col_list)):
+    st.write(col_list[i], "-- Min:", df[col_list[i]].min(), "Max:", df[col_list[i]].max())
 
-st.write('You criteria:', option)
+#start_time = st.slider(
+#    "When do you start?",
+#    value=datetime(2020, 1, 1, 9, 30),
+#    format="MM/DD/YY - hh:mm",
+#)
+#st.write("Start time:", start_time)
 
 #show_btn = st.button("Display!")
 #if show_btn:
-for i in ('gender', 'age_group', 'bmi_group', 'region'):
-    if i in option:
-        # Create distplot with custom bin_size
-        fig = px.pie(region[option].value_counts(), values=region[option].value_counts().values, names=region[option].value_counts().index)
-        fig.update_traces(hoverinfo='label+value', textinfo='label+value+percent')
+#for i in ('gender', 'age_group', 'bmi_group', 'region'):
+#    if i in option:
+#        # Create distplot with custom bin_size
+#        fig = px.pie(region[option].value_counts(), values=region[option].value_counts().values, names=region[option].value_counts().index)
+#        fig.update_traces(hoverinfo='label+value', textinfo='label+value+percent')
+#
+#        # Plot!
+#        st.plotly_chart(fig)
 
-        # Plot!
-        st.plotly_chart(fig)
-
-st.markdown("Last updated: Nov 19, 2024.")
